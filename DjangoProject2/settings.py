03ead14c61517bb89cc8fa8ae0db57109e562f99
batcoder101213 <77876854+batcodez131003@users.noreply.github.com>
2026-05-03 18:57:31 +0530

    Update settings.py

--- DjangoProject2/settings.py
+++ DjangoProject2/settings.py
@@ -128,8 +128,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sejzzu.lohar@gmail.com'  # Replace with your Gmail email address
-EMAIL_HOST_PASSWORD = 'eeikqjbckokwarbc'  # Replace with your Gmail password or app-specific password
+EMAIL_HOST_USER = 'username'  # Replace with your Gmail email address
+EMAIL_HOST_PASSWORD = 'password'  # Replace with your Gmail password or app-specific password
 DEFAULT_FROM_EMAIL = 'Your App <lowpoceat@gmail.com>'  # Replace with your desired "From" email address
 
 LOGIN_REDIRECT_URL = '/'
