393a8d77dd115c82bff689dbc734ccb3ad7f2b2a
Abrar <77876854+batcodez131003@users.noreply.github.com>
2026-06-15 22:41:26 +0530

    Update settings.py

--- DjangoProject2/settings.py
+++ DjangoProject2/settings.py
@@ -133,10 +133,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'theworldf663@gmail.com'
-EMAIL_HOST_PASSWORD = 'rnrlglfbpshqfbdw'
-DEFAULT_FROM_EMAIL = 'theworldf663@gmail.com'
+EMAIL_USE_TLS = 
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
+DEFAULT_FROM_EMAIL = 
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
